f90e25149dd6c8bb64d7ad4bf83dfaefcfd043b0
Timofey Kirillov <timofey.kirillov@flant.com>
2018-10-29 15:25:17 +0300

    Fix go build: deployment.go

--- pkg/monitor/deployment.go
+++ pkg/monitor/deployment.go
@@ -1,10 +1,12 @@
 package monitor
 
+import "k8s.io/client-go/kubernetes"
+
 type DeploymentWatchFeed interface {
 	DeploymentReady() error
 	// ...
 }
 
 func MonitorDeployment(name, namespace string, kube kubernetes.Interface, wf DeploymentWatchFeed, opts WatchOptions) error {
-	// blocking
+	return nil
 }
