693b7b5751d24ce79ec3d09dd97b308b52526da7
Timofey Kirillov <timofey.kirillov@flant.com>
2020-11-25 21:05:01 +0300

    Fix broken multitracker output due to bug in the colorize arguments passing

--- pkg/utils/color.go
+++ pkg/utils/color.go
@@ -8,23 +8,23 @@ import (
 )
 
 func BoldString(format string, a ...interface{}) string {
-	return colorString(colorStyle(color.Bold), format, a)
+	return colorString(colorStyle(color.Bold), format, a...)
 }
 
 func BlueString(format string, a ...interface{}) string {
-	return colorString(colorStyle(color.FgBlue), format, a)
+	return colorString(colorStyle(color.FgBlue), format, a...)
 }
 
 func YellowString(format string, a ...interface{}) string {
-	return colorString(colorStyle(color.FgYellow), format, a)
+	return colorString(colorStyle(color.FgYellow), format, a...)
 }
 
 func GreenString(format string, a ...interface{}) string {
-	return colorString(colorStyle(color.FgGreen), format, a)
+	return colorString(colorStyle(color.FgGreen), format, a...)
 }
 
 func RedString(format string, a ...interface{}) string {
-	return colorString(colorStyle(color.FgRed), format, a)
+	return colorString(colorStyle(color.FgRed), format, a...)
 }
 
 func colorStyle(attrs ...color.Attribute) *style.Style {
