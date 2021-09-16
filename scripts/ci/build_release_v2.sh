5faa5cbb4714cc1752cc7bd30e268e4868a782eb
Timofey Kirillov <timofey.kirillov@flant.com>
2021-09-16 17:13:04 +0300

    infra: fix release build script: disable CGO dependencies

--- scripts/ci/build_release_v2.sh
+++ scripts/ci/build_release_v2.sh
@@ -8,6 +8,8 @@ if [ -z "$VERSION" ] ; then
     exit 1
 fi
 
+export CGO_ENABLED=0
+
 gox -osarch="linux/amd64 linux/arm64 darwin/amd64 darwin/arm64 windows/amd64" \
     -output="release-build/$VERSION/{{.OS}}-{{.Arch}}/bin/kubedog" \
     -ldflags="-s -w -X github.com/werf/kubedog.Version=$VERSION" \
