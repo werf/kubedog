1a8d88e60ac5706d638d685091b59fd7a6241036
Timofey Kirillov <timofey.kirillov@flant.com>
2021-09-16 11:37:10 +0300

    infra: fix release build script for trdl
    Use bin dir in trdl release structure.

--- scripts/ci/build_release_v2.sh
+++ scripts/ci/build_release_v2.sh
@@ -9,6 +9,6 @@ if [ -z "$VERSION" ] ; then
 fi
 
 gox -osarch="linux/amd64 linux/arm64 darwin/amd64 darwin/arm64 windows/amd64" \
-    -output="release-build/$VERSION/{{.OS}}-{{.Arch}}/kubedog" \
+    -output="release-build/$VERSION/{{.OS}}-{{.Arch}}/bin/kubedog" \
     -ldflags="-s -w -X github.com/werf/kubedog.Version=$VERSION" \
         github.com/werf/kubedog/cmd/kubedog
